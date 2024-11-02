0c17df2476782300e2ceb2c109ba325d933127a6
730fry <730fry@gmail.com>
2024-11-02 16:24:23 +0100

    pNavLinkTermsの作成

---
+++ src/components/parts/pNavLinkTerms.vue
@@ -0,0 +1,25 @@
+<script setup>
+  import cAnker from '@/components/commons/cAnker.vue'
+  
+	const prop = defineProps({ 
+		/** @param {String} フォントカラー */ 
+		color: { type: String, default: 'main' } // デフォルトの色を設定
+	});
+</script>
+
+
+<template>
+  <div class="pNavLinkTerms">
+    <cAnker class="pNavLinkTerms-elem" href="/" linkType="INNER" :color="color">プライバシーポリシー</cAnker>
+    <cAnker class="pNavLinkTerms-elem" href="/" linkType="INNER" :color="color">免責事項</cAnker>
+    <cAnker class="pNavLinkTerms-elem" href="/" linkType="INNER" :color="color">反社会勢力に対する基本方針</cAnker>
+  </div>
+</template>
+
+<style lang="scss">
+  .pNavLinkTerms {
+    .pNavLinkTerms-elem {
+      @include fontEng;
+    }
+  }
+</style>
