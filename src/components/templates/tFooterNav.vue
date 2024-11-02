d3bf5da84b98f63cc73fb84be59d7cf48776452d
730fry <730fry@gmail.com>
2024-11-02 17:24:05 +0100

    tFooterNavの作成

---
+++ src/components/templates/tFooterNav.vue
@@ -0,0 +1,34 @@
+<script setup>
+  import pHeadLogo from '@/components/parts/pHeadLogo.vue'
+  import pNavCompanyLink from '@/components/parts/pNavCompanyLink.vue'
+  import pNavButtonLink from '@/components/parts/pNavButtonLink.vue'
+	
+  const prop = defineProps({ 
+		
+	});
+</script>
+
+
+<template>
+  <div class="tFooterNav">
+    <pHeadLogo class=""
+      fileName="vd_logo_lrg.png"
+    />
+    <div class="tFooterNav-container">
+      <pNavButtonLink class=""/>
+      <pNavCompanyLink class=""/>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+  .tFooterNav {
+    .tFooterNav-container {
+      @include mq('LARGE'){
+        display: flex;
+        flex-direction: row-reverse;
+        justify-content: space-between;
+      }
+    }
+  }
+</style>
