5e69b6de54fd313ef2331461c4597c9367f0d2cb
730fry <730fry@gmail.com>
2024-11-02 16:24:55 +0100

    pSectionArea の作成

---
+++ src/components/parts/pSectionArea.vue
@@ -0,0 +1,57 @@
+<script setup>
+	const prop = defineProps({ 
+
+		/** @param {String} 背景色 */
+		background : { type: String, default: ''},
+
+  });
+
+   /**
+     * 関数
+     * 背景色のクラス名を返す
+     * @returns {string} 背景のクラス名
+     */
+    const backgroundType = () => {
+      switch(prop.background) {
+        case 'ダーク':
+          return 'dark';
+        case '':
+        default:
+          return ''; 
+      }
+    }
+</script>
+
+
+<template>
+	<section 
+    :class="['pSectionArea', `pSectionArea-bg--${backgroundType()}`]"
+    >
+    <div class="pSectionArea-container">
+      <slot></slot>
+    </div>
+  </section>
+</template>
+
+
+<style lang="scss">
+	.pSectionArea {
+		&.pSectionArea-bg--dark {
+      background-color: $backgroundB;
+    }
+
+    .pSectionArea-container {
+      margin-left: auto;
+      margin-right: auto;
+
+      @include mq('SHORT'){ 
+        width: 95%;
+      }
+
+      @include mq('LARGE'){
+        width: 95%;
+        max-width: 1230px;
+      }
+    }
+	}
+</style>
