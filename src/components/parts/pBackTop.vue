7b3dfbf87183c3c4795d38581e91b940678dbe65
730fry <730fry@gmail.com>
2024-11-02 16:20:50 +0100

    pBackTopの作成

---
+++ src/components/parts/pBackTop.vue
@@ -0,0 +1,43 @@
+<script setup>
+
+</script>
+
+
+
+<template>
+	<div class="pBackTop">
+		<img class="pBackTop-arrow" src="/assets/images/arrowUp.svg" alt="">
+		<span class="pBackTop-text">
+			<span class="pBackTop-text-elem">back to top</span>
+		</span>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+	.pBackTop {
+		display: flex;
+		align-items: center;
+		@include mq('SHORT'){
+			column-gap: 1.2em;
+		}
+
+		@include mq('LARGE'){
+			column-gap: 1.6em;
+		}
+		
+
+		.pBackTop-arrow {
+			width: 1em;
+		}
+
+		.pBackTop-text {
+			.pBackTop-text-elem {
+				text-transform: uppercase;
+				@include fontEng;
+				@include mqFontSize(1.2em, 1.1em);
+				font-weight: 600;
+				color: $textP;
+			}
+		}
+	}
+</style>
