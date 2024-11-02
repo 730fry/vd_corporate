9ee4c5be0a91d96ae92723f40b8aa2c045249592
730fry <730fry@gmail.com>
2024-11-02 16:25:07 +0100

    pFooterLabelの作成

---
+++ src/components/templates/tFooterLabel.vue
@@ -0,0 +1,26 @@
+<script setup>
+  import pHeadButton from '@/components/parts/pHeadButton.vue'
+  import pNavLink from '@/components/parts/pNavLink.vue'
+	const prop = defineProps({ 
+		
+	});
+</script>
+
+
+<template>
+  <div class="tFooterLabel">
+    <pNavLink/>
+    <pHeadButton
+      href="/contact"
+		  linkType="INNER"
+    >Contact</pHeadButton>
+  </div>
+</template>
+
+<style lang="scss">
+  .tHeaderLabel {
+    display: flex;
+    align-items: center;
+    column-gap: 1em;
+  }
+</style>
