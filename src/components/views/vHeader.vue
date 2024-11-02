74e9e4912dc05cd4ec39b0983be3570757e7a4fd
730fry <730fry@gmail.com>
2024-11-02 16:25:43 +0100

    vHeaderの作成

---
+++ src/components/views/vHeader.vue
@@ -0,0 +1,26 @@
+<script setup>
+import pHeadLogo from '@/components/parts/pHeadLogo.vue'
+import tHeaderLabel from '~/components/templates/tHeaderLabel.vue'
+const prop = defineProps({})
+</script>
+
+<template>
+  <div class="vHeader">
+    <pHeadLogo fileName="vd_logo_lrg.png" />
+    <tHeaderLabel />
+  </div>
+</template>
+
+<style lang="scss">
+@use '@/assets/styles/props' as *;
+
+.vHeader {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-top: 2em;
+  padding-bottom: 2em;
+
+  @include border();
+}
+</style>
