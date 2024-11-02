450972e862b20571a0fe3dde57db65b1bd26b53e
730fry <730fry@gmail.com>
2024-11-02 17:23:31 +0100

    pCopyrightの作成

---
+++ src/components/parts/pFooterCopyright.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+</script>
+
+
+
+<template>
+	<div class="pFooterCopyright">
+		<span class="pFooterCopyright-text">
+      © visionary design inc.
+		</span>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+	.pFooterCopyright {
+		.pFooterCopyright-text {
+      color: $textP;
+      text-transform: uppercase;
+      @include fontEng;
+      @include mqFontSize(1.3em, 1.2em);
+    }
+	}
+</style>
