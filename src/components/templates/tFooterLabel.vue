bc821ffd3aac8ab38f1c599eb00e77edf1f72976
730fry <730fry@gmail.com>
2024-11-02 17:23:56 +0100

    tFooterLabelの作成

--- src/components/templates/tFooterLabel.vue
+++ src/components/templates/tFooterLabel.vue
@@ -1,7 +1,8 @@
 <script setup>
-  import pHeadButton from '@/components/parts/pHeadButton.vue'
-  import pNavLink from '@/components/parts/pNavLink.vue'
-	const prop = defineProps({ 
+  import pNavLinkTerms from '@/components/parts/pNavLinkTerms.vue'
+  import pFooterCopyright from '@/components/parts/pFooterCopyright.vue'
+	
+  const prop = defineProps({ 
 		
 	});
 </script>
@@ -9,18 +10,35 @@
 
 <template>
   <div class="tFooterLabel">
-    <pNavLink/>
-    <pHeadButton
-      href="/contact"
-		  linkType="INNER"
-    >Contact</pHeadButton>
+    <pNavLinkTerms class="tFooterLabel-link"/>
+    <pFooterCopyright class="tFooterLabel-copyright"/>
   </div>
 </template>
 
 <style lang="scss">
-  .tHeaderLabel {
-    display: flex;
-    align-items: center;
-    column-gap: 1em;
+  .tFooterLabel {
+    @include border($show-top: true, $show-bottom: false);
+    @include mq('SHORT'){
+      padding-top: 2em;
+      padding-bottom: 2em;
+    }
+
+    @include mq('LARGE'){
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding-top: 4em;
+      padding-bottom: 4em;
+    }
+
+    .tFooterLabel-link {
+      @include mq('SHORT'){
+        flex-direction: column;
+      }
+    }
+
+    .tFooterLabel-copyright {
+
+    }
   }
 </style>
