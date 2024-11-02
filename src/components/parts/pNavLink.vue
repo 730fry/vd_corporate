9b04468e4ebeae52a9655d65db8126ed70c87e48
730fry <730fry@gmail.com>
2024-11-02 16:24:06 +0100

    pNavLink作成

---
+++ src/components/parts/pNavLink.vue
@@ -0,0 +1,26 @@
+<script setup>
+  import cAnker from '@/components/commons/cAnker.vue'
+  
+	const prop = defineProps({ 
+		
+	});
+</script>
+
+
+<template>
+  <div class="pNavLink">
+    <cAnker class="pNavLink-elem" href="/about" linkType="INNER">About Us</cAnker>
+    <cAnker class="pNavLink-elem" href="/works" linkType="INNER">Works</cAnker>
+    <cAnker class="pNavLink-elem" href="/Lab" linkType="INNER">Lab</cAnker>
+    <cAnker class="pNavLink-elem" href="/Company" linkType="INNER">Company</cAnker>
+  </div>
+</template>
+
+<style lang="scss">
+  .pNavLink {
+    display: flex;
+    align-items: center;
+    column-gap: 1em;
+    @include fontEng;
+  }
+</style>
