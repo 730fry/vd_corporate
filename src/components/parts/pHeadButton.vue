f15b3ebf8e8a1b522dfce5af9e131a06f5f15105
730fry <730fry@gmail.com>
2024-11-02 16:22:57 +0100

    pHeadButtonの作成

---
+++ src/components/parts/pHeadButton.vue
@@ -0,0 +1,35 @@
+<script setup>
+  import cAnker from '@/components/commons/cAnker.vue'
+
+	const prop = defineProps({ 
+		/** @param {String} リンク先 */ 
+		href: String,
+	});
+
+	const getHref = () => {
+		return prop.href
+	}
+
+</script>
+
+
+<template>
+	<cAnker
+		class="pHeadButton"
+		color="basic"
+		:href="getHref()" 
+	  >
+		<slot>click</slot>
+	</cAnker>
+</template>
+
+
+<style lang="scss">
+	.pHeadButton {
+		@include fontEng;
+		display: inline-block;
+    border-radius: 30px;
+		padding: 0.3em 1em;
+		@include gradationP;
+	}
+</style>
